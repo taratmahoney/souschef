98bf7bf1d8ec1ad3d25b919be9cc157cc8fac6d5
Tara Mahoney <tara.mahoney@me.com>
2015-05-29 16:01:18 -0400

    Got pull-out nav working

--- app/assets/javascripts/hamburgers.js
+++ app/assets/javascripts/hamburgers.js
@@ -1,13 +1,13 @@
 $(document).on('ready page:load', function() {
-  
+  console.log("Im loaded")
   $('#nav-toggle').on('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
     $(this).add('.sidebar').toggleClass('is-open');
+    $(".ham-layer#2").toggle();
+    $(".ham-layer#1").toggleClass("rotate-left");
+    $(".ham-layer#3").toggleClass("rotate-right");
     // $('.sidebar').toggleClass('is-open');
-  });
-
-  $('body').on('click', function() {
-    $('.sidebar.is-open, #nav-toggle.is-open').removeClass('is-open');
+    console.log("Now I'm loaded hehehehe")
   });
 });
